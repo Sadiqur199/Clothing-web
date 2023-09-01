dd7491472f5113f9222c61bb6476ff09c70a8e9e
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-09-01 21:55:06 +0600

    install all dependency

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>E-Commerce Website</h1>
+ 
+    </main>
+  )
+}
