3f09cee2a10675d7290f9b56fa69b048a3aa0ff8
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-09-03 04:33:08 +0600

    modify nav section

--- src/Components/CommonModal/CommonModal.jsx
+++ src/Components/CommonModal/CommonModal.jsx
@@ -10,7 +10,7 @@ export default function CommonModal({
   buttonComponent,
   show,
   setShow,
-  showModalTitle
+  showModalTitle,
 }) {
   return (
     <Transition.Root show={show} as={Fragment}>
@@ -41,11 +41,11 @@ export default function CommonModal({
                 <Dialog.Panel className={"w-screen max-w-md"}>
                   <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                      {
-                        showModalTitle ?  <div className="flex items-start justify-between">
-                        <Dialog.Title>{modalTitle}</Dialog.Title>
-                      </div> : null
-                      }
+                      {showModalTitle ? (
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title>{modalTitle}</Dialog.Title>
+                        </div>
+                      ) : null}
                       <div className="mt-20">{mainContent}</div>
                     </div>
                     {showButtons ? (
